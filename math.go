29b181c036eefe06e5d8ef2f04f4188c1b9e35e5
Victor Jansen <victorfjansen@gmail.com>
2023-09-09 18:35:23 -0300

    feat(ci): :construction_worker: adiciona workflow testes
    fix: :bug: acerta bug nos testes
    
    fix(math): ajusta imports

--- math.go
+++ math.go
@@ -1,9 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
-	fmt.Println(Soma(10,10))
+	fmt.Println(soma(112, 10))
 }
 
-func Soma(a int, b int) int {
-	return a + b;
+func soma(a int, b int) int {
+	return a + b
 }
--- math_test.go
+++ math_test.go
@@ -1,12 +1,12 @@
 package main
 
+import "testing"
+
 func TestSoma(t *testing.T) {
-	total := Soma(15,15);
+
+	total := soma(15, 15)
+
 	if total != 30 {
-		t.Errorf(
-			"Resultado da soma eh invalido: resilvado %d. Esperado: %d",
-			total,
-			30
-		)
+		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
